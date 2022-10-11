9a3d55aac6e40bafd5913806f49dc5ee923c8faf
Rakibhasaniu <rakibhasanoyoniu@gmail.com>
2022-10-11 16:03:11 +0600

    path created

--- src/App.js
+++ src/App.js
@@ -2,13 +2,39 @@ import logo from './logo.svg';
 import './App.css';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import Main from './layout/Main';
+import Home from './components/Home/Home'
+import Topics from './components/Topics/Topics'
+import Statictics from './components/Statistics/Statictics';
+import Question from './components/Question/Question';
 
 const router = createBrowserRouter([
   {
     path:'/',
-    element:<Main></Main>
+    element:<Main></Main>,
+    children: [
+      {
+        path:'/',
+        element: <Home></Home>
+      },
+      {
+        path:'/home',
+        element: <Home></Home>
+      },
+      {
+        path:'/topics',
+        element: <Topics></Topics>
+      },
+      {
+        path:'/statictics',
+        element: <Statictics></Statictics>
+      },
+      {
+        path:'/question',
+        element: <Question></Question>
+      }
+    ]
   },
-  
+
 ])
 
 function App() {
