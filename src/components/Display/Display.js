039aa367076d66e85f8764a21e25f3cf2ea67c3b
Rakibhasaniu <rakibhasanoyoniu@gmail.com>
2022-10-12 17:16:26 +0600

    home page responsive

--- src/components/Display/Display.js
+++ src/components/Display/Display.js
@@ -7,12 +7,13 @@ const Display = ({quiz}) => {
     return (
         <div>
         <img
-          className='bg-slate-900	'
+          className=' bg-slate-500 object-cover w-full h-56 mb-6 rounded shadow-lg md:h-64 xl:h-80'
           src={logo}
           alt=''
         />
         <p className='mb-2 text-xl font-bold leading-none sm:text-2xl'>{name}</p>
-       <Link to={`/quiz/${id}`}><button 
+       <Link to={`/quiz/${id}`}><button
+        className='px-8 block w-full mt-4 py-3 font-semibold rounded-full bg-cyan-200 text-gray-800 hover:bg-cyan-400' 
         >
           Start Quiz
         </button></Link>
